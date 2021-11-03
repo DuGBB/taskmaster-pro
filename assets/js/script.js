6348d84897f1b571d9c907cb3c3c0bc8f6abb768
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-03 14:17:02 -0500

    add jQuery ui's datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,11 +144,18 @@ $(".list-group").on("click", "span", function() {
 
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   dateInput.trigger("focus");
 
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
     .val()
     .trim();
@@ -202,6 +209,10 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // remove all tasks
